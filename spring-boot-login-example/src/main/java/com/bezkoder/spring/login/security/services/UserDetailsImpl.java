e868af1bae1d56961c4cf21a7b69f014ceff896b
KhaoulaElfatimi <93163622+khaoula1e@users.noreply.github.com>
2023-06-01 00:56:34 +0100

    backend

---
+++ spring-boot-login-example/src/main/java/com/bezkoder/spring/login/security/services/UserDetailsImpl.java
@@ -0,0 +1,90 @@
+package com.bezkoder.spring.login.security.services;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.bezkoder.spring.login.models.User;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private String username;
+    private String password;
+    private String email;
+    private Long id;
+    
+    public UserDetailsImpl(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    {/*public UserDetailsImpl(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }*/}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Implement setter methods...
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword()
+        );
+    }
+
+}
